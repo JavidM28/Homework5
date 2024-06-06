d8fd5bb222a7b863c831c88229868d0f968413b4
Javid <sairexm@gmail.com>
2024-06-06 16:16:34 +0400

    Super Final Corrected Commit

--- src/Main.java
+++ src/Main.java
@@ -32,11 +32,17 @@ public class Main {
         //TASK 3
         int year = 1900;
         if (year < 1584)
+        {
             System.out.println(year + " год не является високосным.");
+        }
         else if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        {
             System.out.println(year + " год является високосным.");
+        }
         else
+        {
             System.out.println(year + " год не является високосным.");
+        }
 
         //TASK 4
         int deliveryDistance = 95;
